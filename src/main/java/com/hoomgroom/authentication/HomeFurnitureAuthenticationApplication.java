bc01e7982b99ba7ad3b988045d252fd947ce84bd
Akmal76 <akmalramadhan76@gmail.com>
2024-04-04 00:44:10 +0700

    [INIT] Initiate new SpringBoot project

---
+++ src/main/java/com/hoomgroom/authentication/HomeFurnitureAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.hoomgroom.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeFurnitureAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeFurnitureAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoomgroom/authentication/HomeFurnitureAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoomgroom.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFurnitureAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
