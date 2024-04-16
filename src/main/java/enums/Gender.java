c660103e2c4aa6d3dca67163045944dd65294fdc
Akmal76 <akmalramadhan76@gmail.com>
2024-04-16 10:22:47 +0700

    [FEAT] Create User model, Gender and Role enums

---
+++ src/main/java/enums/Gender.java
@@ -0,0 +1,13 @@
+package enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Gender {
+    MALE("MALE"),
+    FEMALE("FEMALE");
+
+    private final String value;
+
+    private Gender(String value) { this.value = value; }
+}
