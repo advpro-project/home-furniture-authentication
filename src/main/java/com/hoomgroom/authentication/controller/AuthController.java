3cb76d2fef62c4a5daf8168b1bf28a45870eea5c
Akmal76 <akmalramadhan76@gmail.com>
2024-04-16 10:47:52 +0700

    [FEAT] Create AuthController

---
+++ src/main/java/com/hoomgroom/authentication/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.hoomgroom.authentication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/")
+    public String authPage(Model model){
+        return "authPage";
+    }
+}
