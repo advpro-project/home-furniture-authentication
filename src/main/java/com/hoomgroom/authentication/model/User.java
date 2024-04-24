e4b96e3364d130bf40a49e0a0e5c867581d59216
Akmal76 <akmalramadhan76@gmail.com>
2024-04-24 19:33:59 +0700

    [FIX] Add Jakarta Persistance dependency

--- src/main/java/com/hoomgroom/authentication/model/User.java
+++ src/main/java/com/hoomgroom/authentication/model/User.java
@@ -5,16 +5,36 @@ import enums.Role;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
+@Entity
 @Getter @Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name")
     private String fullName;
+
+    @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    @Column(unique = true)
     private String username;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Column(name = "wallet_balance")
     private double walletBalance;
 }
