1b9a589232f06193f83bf35c7f02c47a7514721d
Akmal76 <akmalramadhan76@gmail.com>
2024-04-24 18:48:05 +0700

    [GREEN] add User repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,12 +25,18 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	runtimeOnly("org.postgresql:postgresql")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.h2database:h2")
 }
+
 
 tasks.withType<Test> {
 	useJUnitPlatform()
---
+++ src/main/java/com/hoomgroom/authentication/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.hoomgroom.authentication.repository;
+
+import com.hoomgroom.authentication.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
