53ce2864a9aaa558bc5d4bc4911334168a090f2c
Akmal76 <akmalramadhan76@gmail.com>
2024-04-30 15:35:01 +0700

    [FEAT] Change User model using Jakarta Persistance

--- src/main/java/com/hoomgroom/authentication/model/User.java
+++ src/main/java/com/hoomgroom/authentication/model/User.java
@@ -2,17 +2,27 @@ package com.hoomgroom.authentication.model;
 
 import enums.Gender;
 import enums.Role;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@Getter @Setter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
     private String fullName;
     private LocalDate dateOfBirth;
     private Gender gender;
     private String username;
+    @Id
     private String email;
     private String password;
     private Role role;
