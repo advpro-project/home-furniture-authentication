37f4ef3e9b61e0ff7aac4d720578c27ad927ae93
Akmal76 <akmalramadhan76@gmail.com>
2024-05-01 18:38:38 +0700

    [FIX] Typo in WebController

--- src/main/java/com/hoomgroom/authentication/controller/WebController.java
+++ src/main/java/com/hoomgroom/authentication/controller/WebController.java
@@ -3,14 +3,12 @@ package com.hoomgroom.authentication.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 
 @Controller
-@RequestMapping("/auth")
+@RequestMapping("/")
 public class WebController {
-
-    @GetMapping("")
-    public String authPage(Model model){
-        return "authPage";
+    @GetMapping
+    public String homePage() {
+        return "HelloWorld";
     }
 }
